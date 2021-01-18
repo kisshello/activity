b00010ff0022a53a9a5660aff6399fd08415e164
kisshello <1109279069@qq.com>
2021-01-18 21:32:03 +0800

    新增工作流xml文件，增加log4j日志配置，执行测试方法，在数据库里生成对应的工作流的25张表。配置启动工作流的默认方式和一般方式

---
+++ activity01/src/test/java/com/itheima/test/TestCreate.java
@@ -0,0 +1,28 @@
+package com.itheima.test;
+
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngineConfiguration;
+import org.activiti.engine.ProcessEngines;
+import org.junit.Test;
+
+public class TestCreate {
+
+    /**
+     *使用activity提供的默认方式来创建mysql的表
+     */
+    @Test
+    public void testCreateDbTable(){
+        //需要使用activity提供的工具类processEngines，使用方法getDefaultProcessEngine
+        //getDefaultProcessEngine会默认从resources下读取名为activity.cfg.xml的文件
+//        创建processEngine时，就会创建MySQL的表
+//        默认方式
+        //ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+
+        ProcessEngineConfiguration processEngineConfigurationFromResource =
+                ProcessEngineConfiguration.createProcessEngineConfigurationFromResource("activiti.cfg.xml","processEngineConfiguration");
+
+        //System.out.println(processEngine);
+
+    }
+
+}
